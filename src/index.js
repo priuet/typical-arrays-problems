50102ceb3203a6e737b74d702ba64c930bf9112f
Priuet <nancy56@list.ru>
2020-03-17 11:25:33 +0800

    feat: change Node version to 12.3..0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length === 0 || array.length === 0) return 0;
+  else return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (arguments.length === 0 || array.length === 0) return 0;
+  else return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length === 0 || array.length === 0) return 0;
+  else {
+      const length = array.length;
+      array = array.reduce((a, b) => a + b);
+      return array / length;
+  }
 }
